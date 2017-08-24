d7e3f14dc00945e187c75c5839eca504e3019fe4
叶书 <jack.mf@alibaba-inc.com>
2017-08-24 23:19:16 +0800

    Test1的修改

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -3,5 +3,13 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("1");
 	}
 }
+
+
+                   
+
+
+
+                          
